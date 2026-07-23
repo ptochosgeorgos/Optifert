320e270b1ffa9a935859b6c78580acf1d40875e3
Hieronymus <marc.perez.95@protonmail.ch>
2026-07-23 15:48:13 +0200

    Implement Time-Weighted Average (AUC) via linear interpolation for seasonal EEA

--- test_season.R
+++ test_season.R
@@ -21,6 +21,9 @@ df_season <- df_pca |>
     PC1_last = last(PC1),
     PC2_first = first(PC2),
     PC2_last = last(PC2),
+    PC1_AUC = sum((PC1 + lag(PC1)) / 2 * as.numeric(Datum - lag(Datum)), na.rm = TRUE),
+    PC2_AUC = sum((PC2 + lag(PC2)) / 2 * as.numeric(Datum - lag(Datum)), na.rm = TRUE),
+    Season_length_days = as.numeric(last(Datum) - first(Datum)),
     Season_Precip_mm = sum(Cum_Precip_mm[-1], na.rm = TRUE),
     Season_N_Entzug = sum(Interval_N_Entzug_kg_ha[-1], na.rm = TRUE),
     .groups = "drop"
@@ -29,10 +32,12 @@ df_season <- df_pca |>
     Delta_Nmin = Nmin_last - Nmin_first,
     Delta_PC1 = PC1_last - PC1_first,
     Delta_PC2 = PC2_last - PC2_first,
+    PC1_TWA = PC1_AUC / Season_length_days,
+    PC2_TWA = PC2_AUC / Season_length_days,
     Delta_Nmin_kgha = Delta_Nmin * 4.5,
     dN_season = Delta_Nmin_kgha - Season_N_Entzug
   ) |>
-  drop_na(dN_season, Delta_PC1, Delta_PC2, Season_Precip_mm, ParzNrFeld)
+  drop_na(dN_season, PC1_TWA, PC2_TWA, Season_Precip_mm, ParzNrFeld)
 
 cat("Anzahl Reihen in df_season: ", nrow(df_season), "\n")
 
@@ -40,9 +45,9 @@ cat("Anzahl Reihen in df_season: ", nrow(df_season), "\n")
 model_base <- lmer(dN_season ~ Season_Precip_mm + I(Season_Precip_mm^2) + (1 | ParzNrFeld), data = df_season)
 df_season$residual <- resid(model_base)
 
-model_resid <- lm(residual ~ Delta_PC1 + Delta_PC2, data = df_season)
+model_resid <- lm(residual ~ PC1_TWA + PC2_TWA, data = df_season)
 summary(model_resid)
 
 # Anderes Modell fitten
-model_full <- lmer(dN_season ~ Delta_PC1 + Delta_PC2 + Season_Precip_mm + I(Season_Precip_mm^2) + (1 | ParzNrFeld), data = df_season)
+model_full <- lmer(dN_season ~ PC1_TWA + PC2_TWA + Season_Precip_mm + I(Season_Precip_mm^2) + (1 | ParzNrFeld), data = df_season)
 summary(model_full)
