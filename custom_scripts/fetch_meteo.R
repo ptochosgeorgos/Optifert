5e671b964d897cb768a702dc66a291e9127e3d40
Hieronymus <marc.perez.95@protonmail.ch>
2026-09-22 07:45:40 +0200

    Update Lysimeter modeling and render Quarto docs

--- custom_scripts/fetch_meteo.R
+++ custom_scripts/fetch_meteo.R
@@ -9,7 +9,7 @@ fetch_meteo <- function(lat, lon, start_date, end_date) {
     Temp_mean = res$daily$temperature_2m_mean,
     Precip_mm = res$daily$precipitation_sum
   ) |>
-    # Calculate daily GDD (base temperature = 10 for Maize)
-    dplyr::mutate(GDD = ifelse(Temp_mean > 10, Temp_mean - 10, 0))
+    # Calculate daily GDD (base temperature = 6 for Maize)
+    dplyr::mutate(GDD = ifelse(Temp_mean > 6, Temp_mean - 6, 0))
   return(meteo_df)
 }
